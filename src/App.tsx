a5475e7d0708ff762c2acf280c703db6b9cb5bc2
Antonio Victor <Antoniovictor12@live.com>
2022-12-24 17:59:37 -0300

    feat: implementação inicial do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import { Menu } from "./components/Menu";
+
 export function App() {
-  return <h1 className="text-red-700">App</h1>;
+  return (
+    <div
+      className="min-h-screen bg-[#FAFAFA] dark:bg-[#171823] flex 
+    justify-center bg-[url('/bg-light.png')] dark:bg-[url('/bg-dark.png')]
+    bg-no-repeat bg-[auto_200px]"
+    >
+      <main className="max-w-[540px] w-full px-6">
+        <Menu />
+      </main>
+    </div>
+  );
 }
