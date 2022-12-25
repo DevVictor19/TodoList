04d831b33e25dc7708dc3e6f8ec5e9a886c9f0b7
Antonio Victor <Antoniovictor12@live.com>
2022-12-25 16:54:21 -0300

    feat: adicionar interface do auth context

---
+++ src/interfaces/authContextInterface.ts
@@ -0,0 +1,8 @@
+import { User, UserCredential } from "firebase/auth";
+
+export type CurrentUser = User | null;
+
+export interface AuthContextInterface {
+  currentUser: CurrentUser;
+  signup: (email: string, password: string) => Promise<UserCredential>;
+}
