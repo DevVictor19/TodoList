9ba74b4ad7110c8c7098ff9adaf613dac9a118f0
Antonio Victor <Antoniovictor12@live.com>
2022-12-25 16:54:45 -0300

    feat: implementar lógica de signup

--- src/components/Signup/index.tsx
+++ src/components/Signup/index.tsx
@@ -1,17 +1,72 @@
-import { Card } from "../Card";
+import { FormEvent, useRef, useState } from "react";
+import { useAuth } from "../../hooks/useAuth";
 
 export function Signup() {
+  const auth = useAuth();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const passwordConfirmRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+    const passwordConfirm = passwordConfirmRef.current?.value;
+
+    if (
+      email === "" ||
+      email === undefined ||
+      password === "" ||
+      password === undefined ||
+      passwordConfirm === "" ||
+      passwordConfirm === undefined
+    ) {
+      setError("Do not leave empty fields");
+      return;
+    }
+
+    if (password !== passwordConfirm) {
+      return setError("Passwords do not match");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      await auth?.signup(email, password);
+    } catch (e) {
+      setError("Failed to create an account");
+      console.log(e);
+    }
+
+    setLoading(false);
+  };
+
+  const handleChange = () => {
+    if (error === "") return;
+
+    setError("");
+  };
+
   return (
     <>
       <h1 className="text-lg font-bold">Sign up and start using it!</h1>
-      <form className="grid gap-4 mt-6">
+      <form
+        onSubmit={handleSubmit}
+        className="grid gap-4 mt-6"
+        onChange={handleChange}
+      >
         <div className="grid gap-0.5 text-slate-900 dark:text-white">
+          {error && <p className="text-red-500 mb-2">{error}</p>}
           <label htmlFor="email">E-mail</label>
           <input
             className="h-10 rounded-[5px] p-2 bg-transparent border-2 border-[#494C6B] 
             outline-none"
             type="text"
             id="email"
+            ref={emailRef}
           />
         </div>
         <div className="grid gap-0.5 text-slate-900 dark:text-white">
@@ -21,6 +76,7 @@ export function Signup() {
             outline-none"
             type="password"
             id="password"
+            ref={passwordRef}
           />
         </div>
         <div className="grid gap-0.5 text-slate-900 dark:text-white">
@@ -30,6 +86,7 @@ export function Signup() {
             outline-none"
             type="password"
             id="confirm-password"
+            ref={passwordConfirmRef}
           />
         </div>
         <div>
@@ -37,6 +94,7 @@ export function Signup() {
             className="h-12 bg-[#4c23d3] hover:bg-[#724deb] active:bg-[#724deb] 
             w-32 rounded text-white font-bold mt-2"
             type="submit"
+            disabled={loading}
           >
             Sign up
           </button>
