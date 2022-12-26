3c1e63cda064072ae2a044b3e11f25e096250f6a
Antonio Victor <Antoniovictor12@live.com>
2022-12-26 20:11:19 -0300

    feat: atualizar imports do app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
-import { Card } from "./components/Card";
-import { Login } from "./components/Login";
 import { Menu } from "./components/Menu";
-import { Signup } from "./components/Signup";
+import { AppRoutes } from "./routes";
 
 export function App() {
   return (
     <div
       className="min-h-screen bg-[#FAFAFA] dark:bg-[#171823] flex 
     justify-center bg-[url('/bg-light.png')] dark:bg-[url('/bg-dark.png')]
-    bg-no-repeat bg-[auto_200px]"
+    bg-no-repeat bg-[auto_240px]"
     >
       <main className="max-w-[540px] w-full px-6">
         <Menu />
-        <Card>
-          <Signup />
-        </Card>
+        <AppRoutes />
       </main>
     </div>
   );
