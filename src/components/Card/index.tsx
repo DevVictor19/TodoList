d4589b8e161f3dc259bb969549b3bf2fb51c5364
Antonio Victor <Antoniovictor12@live.com>
2022-12-26 15:58:00 -0300

    feat: remover estilo preso e adicionar nova prop

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 interface Props {
   children: React.ReactNode;
+  styleClasses?: string;
 }
 
-export function Card({ children }: Props) {
+export function Card({ children, styleClasses }: Props) {
   return (
     <section
-      className="bg-white dark:bg-[#25273D] shadow-[0px_35px_50px_-15px_#c2c3d67f] 
-      dark:shadow-[0px_35px_50px_-15px_#0000007f] rounded-[5px] mt-10 p-5 font-base 
-      text-[#494C6B] dark:text-[#C8CBE7]"
+      className={`bg-white dark:bg-[#25273D] shadow-[0px_35px_50px_-15px_#c2c3d67f] 
+      dark:shadow-[0px_35px_50px_-15px_#0000007f] rounded-[5px] p-5 font-base 
+      text-[#494C6B] dark:text-[#C8CBE7] ${
+        styleClasses && styleClasses.split(" ")
+      }`}
     >
       {children}
     </section>
