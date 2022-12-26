c2dc25283125ce6ddc7418b499f0c8c7019f31ea
Antonio Victor <Antoniovictor12@live.com>
2022-12-26 15:58:44 -0300

    feat: adicionar menudrop down

--- src/components/Menu/Profile/index.tsx
+++ src/components/Menu/Profile/index.tsx
@@ -1,27 +1,12 @@
-import { useState, MouseEvent, useCallback } from "react";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import { UserCircle, SignOut, UserGear } from "phosphor-react";
+import { useCallback } from "react";
+import { UserCircle, SignOut } from "phosphor-react";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import { useAuth } from "../../../hooks/useAuth";
 import { AuthContextInterface } from "../../../interfaces/authContextInterface";
+import { Card } from "../../Card";
 
 export function Profile() {
   const { logout } = useAuth() as AuthContextInterface;
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-
-  const handleClick = useCallback(
-    (event: MouseEvent<HTMLButtonElement>) => {
-      setAnchorEl(event.currentTarget);
-    },
-    [setAnchorEl]
-  );
-
-  const handleClose = useCallback(() => {
-    setAnchorEl(null);
-  }, [setAnchorEl]);
 
   const handleLogout = useCallback(async () => {
     try {
@@ -32,38 +17,27 @@ export function Profile() {
   }, []);
 
   return (
-    <>
-      <button
-        id="basic-button"
-        aria-controls={open ? "basic-menu" : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? "true" : undefined}
-        onClick={handleClick}
-      >
-        <UserCircle size={22} weight="fill" />
-      </button>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-      >
-        <MenuItem onClick={handleClose} dense>
-          <ListItemIcon>
-            <UserGear size={22} weight="fill" />
-          </ListItemIcon>
-          <ListItemText>Profile</ListItemText>
-        </MenuItem>
-        <MenuItem onClick={handleLogout} dense>
-          <ListItemIcon>
-            <SignOut size={22} weight="fill" />
-          </ListItemIcon>
-          <ListItemText>Sign Out</ListItemText>
-        </MenuItem>
-      </Menu>
-    </>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild>
+        <button>
+          <UserCircle size={22} weight="fill" />
+        </button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content sideOffset={5}>
+          <Card>
+            <nav>
+              <DropdownMenu.Item
+                className="flex items-center gap-4 cursor-pointer"
+                onClick={handleLogout}
+              >
+                <SignOut size={20} /> Sign out
+              </DropdownMenu.Item>
+            </nav>
+          </Card>
+          <DropdownMenu.Arrow className="fill-white dark:fill-[#25273D]" />
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
   );
 }
