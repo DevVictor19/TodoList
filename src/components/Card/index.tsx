9f13bde71d94a2c80ff459c657468e62cb45048c
Antonio Victor <Antoniovictor12@live.com>
2022-12-27 10:45:03 -0300

    feat: remover padding e utilizar join nos estilos

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,9 +9,9 @@ export function Card({ children, styleClasses }: Props) {
   return (
     <section
       className={`bg-white dark:bg-[#25273D] shadow-[0px_35px_50px_-15px_#c2c3d67f] 
-      dark:shadow-[0px_35px_50px_-15px_#0000007f] rounded-[5px] p-5 font-base 
+      dark:shadow-[0px_35px_50px_-15px_#0000007f] rounded-[5px] font-base 
       text-[#494C6B] dark:text-[#C8CBE7] ${
-        styleClasses && styleClasses.split(" ")
+        styleClasses && styleClasses.split(" ").join(" ")
       }`}
     >
       {children}
