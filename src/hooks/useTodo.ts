a77edc8af434064ccd91f4c50e56d57c7bc51360
Antonio Victor <Antoniovictor12@live.com>
2022-12-28 20:43:11 -0300

    feat: implementar getTodos e addTodo

---
+++ src/hooks/useTodo.ts
@@ -0,0 +1,36 @@
+import { collection, doc, getDocs, query, setDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { Todo } from "../interfaces/Todo";
+
+export function useTodo(userId: string) {
+  const collectionRef = collection(db, "users", userId, "todos");
+
+  const getTodos = async () => {
+    let dbTodos: Todo[] = [];
+
+    try {
+      const querySnapShot = await getDocs(query(collectionRef));
+
+      querySnapShot.forEach((doc) => {
+        dbTodos.push(doc.data() as Todo);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+
+    return dbTodos;
+  };
+
+  const addTodo = async (newTodo: Todo) => {
+    try {
+      await setDoc(doc(db, "users", userId, "todos", newTodo.id), newTodo);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    getTodos,
+    addTodo,
+  };
+}
