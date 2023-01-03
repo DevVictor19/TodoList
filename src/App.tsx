0419b3c33290ee2ed1fd575ed2497c0ec9023225
Antonio Victor <Antoniovictor12@live.com>
2023-01-03 19:38:49 -0300

    feat: adicionar ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
+import "react-toastify/dist/ReactToastify.min.css";
+import { ToastContainer } from "react-toastify";
 import { Menu } from "./components/Menu";
 import { AppRoutes } from "./routes";
 
 export function App() {
   return (
-    <div
-      className="min-h-screen bg-[#FAFAFA] dark:bg-[#171823] flex 
-    justify-center bg-[url('/bg-light.png')] dark:bg-[url('/bg-dark.png')]
-    bg-no-repeat bg-[auto_240px]"
-    >
-      <main className="max-w-[540px] w-full px-6">
-        <Menu />
-        <AppRoutes />
-      </main>
-    </div>
+    <>
+      <ToastContainer />
+      <div
+        className="min-h-screen bg-[#FAFAFA] dark:bg-[#171823] flex 
+        justify-center bg-[url('/bg-light.png')] dark:bg-[url('/bg-dark.png')]
+        bg-no-repeat bg-[auto_240px]"
+      >
+        <main className="max-w-[540px] w-full px-6">
+          <Menu />
+          <AppRoutes />
+        </main>
+      </div>
+    </>
   );
 }
