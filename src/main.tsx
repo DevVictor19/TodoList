9950e4f4e9befc4f968ab87af06a05e6d538d6f8
Antonio Victor <Antoniovictor12@live.com>
2023-01-03 19:39:18 -0300

    style: organizar imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeContextProvider } from "./contexts/themeContext";
+import { AuthContextProvider } from "./contexts/authContext";
 import { App } from "./App";
 import "./index.css";
-import { AuthContextProvider } from "./contexts/authContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
