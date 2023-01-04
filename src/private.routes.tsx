3a1b47922d4137d319db4d372426ebcb71ee6e4f
Antonio Victor <Antoniovictor12@live.com>
2023-01-04 18:49:33 -0300

    feat: adicionar lazy loading

--- src/private.routes.tsx
+++ src/private.routes.tsx
@@ -1,9 +1,29 @@
+import { lazy, Suspense } from "react";
 import { useAuth } from "./hooks/useAuth";
 import { Outlet } from "react-router-dom";
-import { TodoList } from "./components/TodoList";
+import { Spinner } from "./components/Spinner";
+
+const TodoList = lazy(() =>
+  import("./components/TodoList").then((module) => ({
+    default: module.TodoList,
+  }))
+);
 
 export function PrivateRoutes() {
   const { currentUser } = useAuth();
 
-  return currentUser === null ? <Outlet /> : <TodoList />;
+  return currentUser === null ? (
+    <Outlet />
+  ) : (
+    <Suspense
+      fallback={
+        <Spinner
+          containerClassName="min-h-[200px] flex items-center justify-center"
+          contentClassName="text-transparent fill-[#fff]"
+        />
+      }
+    >
+      <TodoList />
+    </Suspense>
+  );
 }
