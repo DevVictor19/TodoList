346453f29b0017fef07516021bb53a31c85b982a
Antonio Victor <Antoniovictor12@live.com>
2023-01-04 13:36:47 -0300

    feat: adicionar toastContainer e sua config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import "react-toastify/dist/ReactToastify.min.css";
-import { ToastContainer } from "react-toastify";
+import { useScreenWidth } from "./hooks/useScreenWidth";
 import { Menu } from "./components/Menu";
 import { AppRoutes } from "./routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 export function App() {
+  const screenWidth = useScreenWidth();
+
   return (
     <>
-      <ToastContainer />
+      <ToastContainer
+        className="text-sm"
+        limit={screenWidth > 480 ? 3 : 1}
+        theme="colored"
+        autoClose={screenWidth > 480 ? 2000 : 1000}
+      />
       <div
         className="min-h-screen bg-[#FAFAFA] dark:bg-[#171823] flex 
         justify-center bg-[url('/bg-light.png')] dark:bg-[url('/bg-dark.png')]
