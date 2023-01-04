27136ea9a447e993cee5b04408f7fc767b2c7a52
Antonio Victor <Antoniovictor12@live.com>
2023-01-04 18:49:21 -0300

    refactor: remover metodo desnecessario

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -10,9 +10,7 @@ export function Card({ children, styleClasses }: Props) {
     <section
       className={`bg-white dark:bg-[#25273D] shadow-[0px_35px_50px_-15px_#c2c3d67f] 
       dark:shadow-[0px_35px_50px_-15px_#0000007f] rounded-[5px] font-base 
-      text-[#494C6B] dark:text-[#C8CBE7] ${
-        styleClasses && styleClasses.split(" ").join(" ")
-      }`}
+      text-[#494C6B] dark:text-[#C8CBE7] ${styleClasses}`}
     >
       {children}
     </section>
