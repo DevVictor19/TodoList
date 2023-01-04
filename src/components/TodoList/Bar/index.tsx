bf48bf9ed0afaca98d28e02d6a9eb228c67beba3
Antonio Victor <Antoniovictor12@live.com>
2023-01-04 17:08:57 -0300

    feat: adicionar responsivo

--- src/components/TodoList/Bar/index.tsx
+++ src/components/TodoList/Bar/index.tsx
@@ -26,21 +26,21 @@ export function Bar({ onAddTodo }: Props) {
   };
 
   return (
-    <form className="w-full mt-10 relative" onSubmit={handleSubmit}>
+    <form className="w-full mt-10 md:mt-12 relative" onSubmit={handleSubmit}>
       <input
-        className="w-full pl-[52px] pr-5 py-[18px] outline-none rounded-[5px] bg-white 
+        className="w-full pl-[52px] md:pl-[72px] pr-5 py-[18px] outline-none rounded-[5px] bg-white 
         dark:bg-[#25273D] shadow-[0px_35px_50px_-15px_#c2c3d67f] dark:shadow-[0px_35px_50px_-15px_#0000007f] 
         placeholder:text-[#9495A5] dark:placeholder:text-[#767992] text-slate-900 
-        dark:text-white text-xs"
+        dark:text-white text-xs md:text-lg"
         type="text"
         ref={inputRef}
         placeholder="Create a new todo…"
       />
       <button
-        className="w-5 h-5 bg-transparent absolute inset-y-0 my-auto left-5 border-[1px] rounded-full
-        dark:border-[#393A4B] hover:bg-gradient-to-br hover:from-[#55DDFF] hover:to-[#C058F3]
-        active:bg-gradient-to-br active:from-[#55DDFF] active:to-[#C058F3] flex justify-center
-        items-center group"
+        className="w-5 h-5 md:w-6 md:h-6 bg-transparent absolute inset-y-0 my-auto left-5 md:left-6 border-[1px] 
+        rounded-full dark:border-[#393A4B] hover:bg-gradient-to-br hover:from-[#55DDFF] 
+        hover:to-[#C058F3]active:bg-gradient-to-br active:from-[#55DDFF] active:to-[#C058F3] 
+        flex justify-centeritems-center group"
         type="submit"
       >
         <Check
